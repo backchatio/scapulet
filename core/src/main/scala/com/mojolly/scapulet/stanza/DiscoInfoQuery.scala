34d3701f71c0a08fb1be6380648cd84cce7feb0d
Ivan Porto Carrero <ivan@flanders.co.nz>
2010-10-31 00:50:22 +0100

    Adds specs for generating a DiscoInfoQuery

--- core/src/main/scala/com/mojolly/scapulet/stanza/DiscoInfoQuery.scala
+++ core/src/main/scala/com/mojolly/scapulet/stanza/DiscoInfoQuery.scala
@@ -1,13 +1,22 @@
-package com.mojolly.scapulet.stanza
+package com.mojolly.scapulet
+package stanza
 
 import xml._
+import util._
 
 object DiscoInfoQuery {
+
+  import Implicits._
+
   val DISCO_INFO_NS = "http://jabber.org/protocol/disco#info"
 
   val sl = <query xmlns={DISCO_INFO_NS} />
 
   def apply[TNode <: NodeSeq](id: String, from: String, to: String): NodeSeq = {
+    require(id.isNotBlank, "You need to provide an id for this info query")
+    require(from.isNotBlank, "You need to provide a sender address for this disco info query")
+    require(to.isNotBlank, "You need to provide a recipient address for this disco info query")
+
     <iq type="get" id={id} to={to} from={from}>
       {sl}
     </iq>
